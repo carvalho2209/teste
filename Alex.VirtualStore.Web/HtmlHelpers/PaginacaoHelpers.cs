9f65110098bbaae9560ec425cad409a4b282db87
Alexandre Carvalho <carvalho2209@hotmail.com>
2017-09-17 12:45:39 -0700

    teste

---
+++ Alex.VirtualStore.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Alex.VirtualStore.Web.Models;
+
+namespace Alex.VirtualStore.Web.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+            var resultado = new StringBuilder();
+
+            for (int i = 0; i < paginacao.TotalPagina; i++)
+            {
+                var tag = new TagBuilder("a");
+
+                tag.MergeAttribute("href", paginaUrl(i));
+
+                tag.InnerHtml = i.ToString();
+
+                if (i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
---
+++ Alex.VirtualStore.Web/Models/Paginacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Alex.VirtualStore.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; }
+
+        public int ItensPorPagina { get; set; }
+
+        public int PaginaAtual { get; set; }
+
+        public int TotalPagina => (int) Math.Ceiling((decimal) ItensTotal/ItensPorPagina);
+    }
+}
